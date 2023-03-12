cbbb8d9c364134196dead972e484157cdf7bfb42
your email address <your email address>
2023-03-12 21:29:24 +0200

    'start'

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <NavBar /> 
+  <router-view />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import NavBar from './components/NavBarComponent.vue'
 
-nav {
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
+<style>
+/* nav {
   padding: 30px;
 }
 
@@ -26,5 +24,5 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section id="home" class="home h-100">
+      <img class="image" src="https://i.postimg.cc/qBNVZ1gF/image.jpg" alt="">
+      <div class="container-fluid my-auto">
+        <div class="row">
+          <div class="col-6 mx-auto">
+            <h1>Book a Table at Your Favorite Restaurant</h1>
+            <h2>Hassle-free reservations with our online booking system</h2>
+            <button btn btn-primary>Book Now</button>
+          </div>
+        </div>
+      </div>
+  </section>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
+<style scoped>
+  #home {
+    min-width: 100vw;
+    min-height: 100vh;
+    position: relative;
+    background-color: #1f2b21;
+  }
+  .image {
+    position: absolute;
+    left: 0;
+    top: 0;
+    min-width: 100vw;
+    /* min-height: 100vh; */
+    opacity: 0.2;
+  }
+  .col {
+    margin: auto;
+  }
+</style>
